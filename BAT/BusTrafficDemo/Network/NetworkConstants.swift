2f77daf0f909748315dd722913719ff3e5e7258f
Nikola Dojchinovski <nikola.dojchinovski@webfactory.mk>
2018-08-08 17:36:09 +0200

    Refactor

---
+++ BAT/BusTrafficDemo/Network/NetworkConstants.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkConstants.swift
+//  BusTrafficDemo
+//
+//  Created by Nikola Dojchinovski on 8/8/18.
+//  Copyright © 2018 Nikola Dojchinovski. All rights reserved.
+//
+
+import Foundation
+
+struct Network {
+    struct Endpoints {
+        static let baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    }
+    
+    
+}
